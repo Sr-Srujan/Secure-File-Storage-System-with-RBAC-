d1527a1606ff6a9f926fe916556d2aaec3347175
Sr-Srujan <srsrujan702@gmail.com>
2026-08-06 22:26:42 +0530

    Create Role repository

---
+++ backend/src/main/java/com/srujan/backend/repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package com.srujan.backend.repository;
+
+import com.srujan.backend.entity.Role;
+import com.srujan.backend.enums.RoleName;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByName(RoleName name);
+
+    boolean existsByName(RoleName name);
+}

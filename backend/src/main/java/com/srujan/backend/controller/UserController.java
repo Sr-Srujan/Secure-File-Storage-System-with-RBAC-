79ea6517c224217fe50e31f61b5ae63aacc1c0d3
Sr-Srujan <srsrujan702@gmail.com>
2026-08-09 11:35:56 +0530

    Add user registration controller

---
+++ backend/src/main/java/com/srujan/backend/controller/UserController.java
@@ -0,0 +1,53 @@
+package com.srujan.backend.controller;
+
+import com.srujan.backend.dto.RegisterRequest;
+import com.srujan.backend.entity.User;
+import com.srujan.backend.enums.RoleName;
+import com.srujan.backend.entity.Role;
+import com.srujan.backend.repository.RoleRepository;
+import com.srujan.backend.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+    private final RoleRepository roleRepository;
+
+    public UserController(UserService userService,
+                          RoleRepository roleRepository) {
+        this.userService = userService;
+        this.roleRepository = roleRepository;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody RegisterRequest request) {
+
+        if (userService.findByUsername(request.getUsername()).isPresent()) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+
+        if (userService.findByEmail(request.getEmail()).isPresent()) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+
+        User user = new User();
+
+        user.setUsername(request.getUsername());
+        user.setEmail(request.getEmail());
+        user.setPassword(request.getPassword());
+        user.setEnabled(true);
+
+        Role userRole = roleRepository.findByName(RoleName.ROLE_USER)
+                .orElseThrow(() -> new RuntimeException("ROLE_USER not found"));
+
+        user.getRoles().add(userRole);
+
+        User createdUser = userService.createUser(user);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+    }
+}
